6e24f45ea876eb7cf519c38e26724c07a6849036
SikroYang <yxx95lz@163.com>
2026-02-09 13:16:05 +0800

    fix: 更新默认仓库为 JinDexStatic/SikroBucket

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "SikroOrg/SikroBucket:master"
+    [String]$upstream = "JinDexStatic/SikroBucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
